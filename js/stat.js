1837225c0244b164283aa5f4309fe6914adca269
habibui <habib-96@mail.ru>
2020-02-26 00:38:12 +0300

    добавил переменные

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,25 @@
 'use strict';
 
-var renderStatisticsCloud = function (ctx, x, y, color) {
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = 150;
+var BOTTOM_FONT_GAP = 20;
+var TOP_FONT_GAP = 10;
+var FIRST_GAP = 40;
+var REGULAR_GAP = 50;
+
+var firstColumnX = CLOUD_X + FIRST_GAP;
+var regularWidth = BAR_WIDTH + REGULAR_GAP;
+var ColumnY = CLOUD_Y + CLOUD_HEIGHT - BOTTOM_FONT_GAP;
+
+var renderCloud = function (ctx, x, y, color) {
   ctx.save();
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, 420, 270);
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
   ctx.restore();
 };
 
@@ -20,21 +36,21 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderStatisticsCloud(ctx, 100 + 10, 10 + 10, 'rgba(0, 0, 0, 0.3)');
-  renderStatisticsCloud(ctx, 100, 10, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
 
   var maxTime = getMaxElement(times);
 
-  ctx.fillText('Ура вы победили!', 100 + 20, 10 + 30);
-  ctx.fillText('Список результатов:', 100 + 20, 10 + 50);
+  ctx.fillText('Ура вы победили!', CLOUD_X + 20, CLOUD_Y + 30);
+  ctx.fillText('Список результатов:', CLOUD_X + 20, CLOUD_Y + 50);
 
   for (var i = 0; i < players.length; ++i) {
-    ctx.fillText(players[i], 100 + 40 + ((40 + 50) * i), 10 + 270 - 20);
+    ctx.fillText(players[i], firstColumnX + ((regularWidth) * i), ColumnY);
     ctx.fillStyle = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(' + 240 + ',' + 100 * Math.random() + '%, 50%)';
-    ctx.fillRect(100 + 40 + ((40 + 50) * i), 10 + 270 - 20 - 20 - (150 * times[i]) / maxTime, 40, (150 * times[i]) / maxTime);
+    ctx.fillRect(firstColumnX + ((regularWidth) * i), ColumnY - BOTTOM_FONT_GAP - (BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.ceil(times[i]), 100 + 40 + ((40 + 50) * i), 10 + 270 - 20 - 20 - 10 - (150 * times[i]) / maxTime);
+    ctx.fillText(Math.ceil(times[i]), firstColumnX + ((regularWidth) * i), ColumnY - BOTTOM_FONT_GAP - TOP_FONT_GAP - (BAR_HEIGHT * times[i]) / maxTime);
   }
 };
